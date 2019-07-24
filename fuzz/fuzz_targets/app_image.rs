e62791e439d2e122061c678b9f76e444c658c8e6
Jonas Schievink <jonasschievink@gmail.com>
2019-07-24 15:11:32 +0200

    Update fuzz targets to new API

--- fuzz/fuzz_targets/app_image.rs
+++ fuzz/fuzz_targets/app_image.rs
@@ -3,13 +3,14 @@
 extern crate libfuzzer_sys;
 extern crate gbl;
 
-use gbl::{AppImage, Gbl};
+use gbl::{AppImage, Gbl, P256KeyPair};
 
 #[rustfmt::skip] // FIXME: https://github.com/rust-lang/rustfmt/issues/3234
 fuzz_target!(|data: &[u8]| {
 
 	if let Ok(app_img) = AppImage::parse(data) {
-        if let Ok(app_img) = app_img.sign(include_str!("../../test-data/signing-key")) {
+	    let keypair = P256KeyPair::from_pem(include_str!("../../test-data/signing-key")).unwrap();
+        if let Ok(app_img) = app_img.sign(&keypair) {
             assert!(app_img.is_signed());
             Gbl::from_app_image(app_img).to_bytes();
         }
